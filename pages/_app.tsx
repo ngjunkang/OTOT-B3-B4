cc7c648e8cdefbd1e677dd159755735ed1915af7
Ng Jun Kang <ng_jun_kang@hotmail.com>
2022-10-16 02:45:02 +0800

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from "next/app";
 
 import "../styles/globals.css";
 import { AppLayout } from "../components/layouts";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <title>Yellow Book</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <AppLayout>
         <Component {...pageProps} />
       </AppLayout>
